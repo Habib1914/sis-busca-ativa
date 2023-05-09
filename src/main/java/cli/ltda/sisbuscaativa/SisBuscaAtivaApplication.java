414fd07678d0ecd9e07824e5961dab786fb3da2c
LimaIsaias <coelholimaisaias@gmail.com>
2023-05-09 12:19:11 -0300

    Initial commit

---
+++ src/main/java/cli/ltda/sisbuscaativa/SisBuscaAtivaApplication.java
@@ -0,0 +1,13 @@
+package cli.ltda.sisbuscaativa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SisBuscaAtivaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SisBuscaAtivaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/cli/ltda/sisbuscaativa/SisBuscaAtivaApplicationTests.java
@@ -0,0 +1,13 @@
+package cli.ltda.sisbuscaativa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SisBuscaAtivaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
